25a488f759085ceb82df3abc129a32a0070d8b74
Soura21 <95709386+Chakrabortysoura@users.noreply.github.com>
2025-11-06 10:34:23 +0530

    stderr can now be mapped to a different file with command line arguments and the main.c function parses the --err flag too.

--- main.c
+++ main.c
@@ -13,14 +13,15 @@
 int main(int argc, char  *argv[]){
   int total_track_number=6;
   for(int i=1;i<argc;i++){ // Command line args parser to parse through the args
-    if (strcmp(argv[i], "--total")==0){
-      if (i+1<argc){
-        total_track_number=atoi(argv[i+1]);
-        break;
-      }else{
-        fprintf(stderr, "Please provide a valid number of inputs to be mapped after the '--total' flag\n");
-        return 1;
-      }
+    if (strcmp(argv[i], "--total")==0 && i+1<argc){
+      total_track_number=atoi(argv[i+1]);
+      break;
+    }else if (strcmp(argv[i], "--err")==0 && i+1<argc){
+      fclose(stderr);
+      stderr=fopen(argv[i+1], "a");
+    }else{
+      fprintf(stderr, "Please provide the command line arguments correctly\n");
+      return 1;
     }
   }
   
