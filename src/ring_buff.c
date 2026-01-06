a130d83c5254b5e86c6fe74b0ecf1ee157e16f30
Soura21 <chakrabortysoura21@gmail.com>
2026-01-06 11:01:01 +0530

    Ring buffer read and write implementation now correctly reads and write ensuring read or write head doesn't violate the existing data in the buffer and ensuring <=n amount of data is written or read at max.

--- src/ring_buff.c
+++ src/ring_buff.c
@@ -11,7 +11,7 @@ int read_buffer(RingBuffer *ring_buff, float *buff, int n){
   /*
   *  Read upto n amount of data from the ring_buff. This function ensures that we don't read into the part of the buffer
   *  that was previously read by another process and is meant for write. Read only the amount of data so that the read_head doesn't go beyond
-  *  the curr_write head. Return the number of elements read from the ring_buff.
+  *  the curr_write head and the total amount read is still under n. Return the number of elements read from the ring_buff.
   */
   if (buff==NULL || ring_buff==NULL || n==0){
     return 0;
@@ -23,23 +23,25 @@ int read_buffer(RingBuffer *ring_buff, float *buff, int n){
   pthread_mutex_lock(&ring_buff->write_head_lock);
   int curr_write=ring_buff->write_head;
   pthread_mutext_unlock(&ring_buff->write_head_lock);
-  
-  n=((curr_read+n)%MAX_SIZE)<curr_write?n:curr_write-curr_read; // Get the available amount of data in the ringbuffer for reading
-  
-  memcpy(buff, ring_buffer->buff[curr_read], n);
+
+  int i=0;
+  for (i=0;curr_read<curr_write && i<n;i++){
+    buff[i]=ring_buff[curr_read];
+    curr_read=(curr_read+1)%MAX_SIZE;
+  }
 
   pthread_mutex_lock(&ring_buff->write_head_lock);
-  ring_buff->read_head+=n;
+  ring_buff->read_head=curr_read;
   pthread_mutext_unlock(&ring_buff->write_head_lock);
   
-  return n;
+  return i;
 }
 
 int write_buffer(RingBuffer *ring_buff, float *buff, int n){
   /*
   *  Write upto n amount of data to the ring_buff. This function is thread-safe and ensures that we don't write into the part of the buffer
-  *  that is yet to be read. Write only the amount of data in the buff so that the write head doesn't go beyond the current read_head. 
-  *  Return the number of elements written to the ring_buff.
+  *  that is yet to be read. Write only the amount of data in the buff so that the write head doesn't go beyond the current read_head and the amount written is 
+  *  still under n. Return the number of elements written to the ring_buff.
   */
   if (buff==NULL || ring_buff==NULL || n==0){
     return 0;
@@ -52,13 +54,15 @@ int write_buffer(RingBuffer *ring_buff, float *buff, int n){
   int curr_write=ring_buff->write_head;
   pthread_mutext_unlock(&ring_buff->write_head_lock);
 
-  n=((curr_write+n)%MAX_SIZE)<curr_read?n:curr_read-curr_write; // Get the available amount of data in the ringbuffer for write
-  
-  memcpy(ring_buffer->buff[curr_write], buff, n);
+  int i=0;
+  for (i=0;curr_write<curr_read && i<n;i++){
+    ring_buff[curr_write]=buff[i];
+    curr_write=(curr_write+1)%MAX_SIZE;
+  }
   
   pthread_mutex_lock(&ring_buff->write_head_lock);
-  ring_buff->write_head+=n;
+  ring_buff->write_head=curr_write;
   pthread_mutext_unlock(&ring_buff->write_head_lock);
   
-  return n;
+  return i;
 }
