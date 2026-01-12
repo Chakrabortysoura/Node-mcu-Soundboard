05aeda56847a7f1cd9d37db1d75af4b81cd1f08e
Soura21 <chakrabortysoura21@gmail.com>
2026-01-12 11:56:16 +0530

    Switched to using strdup instead of manual calloc for duplicating from source string.

--- src/String.c
+++ src/String.c
@@ -4,7 +4,7 @@
 
 #include <stdlib.h>
 #include <string.h>
-
+#include <errno.h>
 #include "String.h"
 
 String * init_string(){
@@ -13,18 +13,17 @@ String * init_string(){
 
 String * init_string_from_src(const char *src){
   String *result=(String *)calloc(1, sizeof(String));
-  if (result!=NULL){
-    result->size=strlen(src)+10;
+  if (result==NULL){
+    fprintf(stderr, "Error allocating new string object.Error: %s\n", strerror(errno));
+    return NULL;
   }
-  result->str=(char *)calloc(result->size,sizeof(char));
-  if (result->str!=NULL){
-    strcpy(result->str, src);
-    result->len=result->size-10;
-  }else{
-    // Incase of actual string buffer allocation fails due to some internal error with calloc.
-    // In that situation free the object and return null to indicate to the user that the allocation failed.
+  result->str=strdup(src);
+  if (result->str==NULL){
+    fprintf(stderr, "Error duplicating source string. Error: %s\n", strerror(errno));
     free(result);
-    result=NULL;
+    return NULL;
   }
+  result->size=strlen(src)+10;
+  result->len=result->size-10;
   return result; 
 }
